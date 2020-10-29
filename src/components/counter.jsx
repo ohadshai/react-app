ad68aa39bc335e51cb0f551c1f6c0c50dca716cb
ohad <ohad@ohads-Mac-mini.local>
2020-10-29 18:18:32 +0300

    continue app - youtube - 1:30:28

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,24 +1,64 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {  
-      count: 0,
+  state = {
+    value: this.props.value,
+    tags: ["tag1", "tag2", "tag3"],
   };
+
+//   constructor(){
+//     binding methods in constructor  
+//     super();
+//     this.handleIncrement = this.handleIncrement.bind(this); 
+
+//   }
+
   render() {
-    let classes = "badge m-2 badge-"
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    console.log('props', this.props)
     return (
       <React.Fragment>
-        <span className= {classes}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+          
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button><br/>
+        {/* {this.renderTgas()} */}
       </React.Fragment>
     );
   }
 
-  formatCount(){
-      const { count } = this.state
-      return count === 0 ? "Zero": count;
-}
+  handleIncrement = () => {
+    // console.log("Increment Clicked", this)
+    this.setState({value: this.state.value + 1});
+  }
+
+  renderTgas() {
+    return this.state.tags.length === 0 ? (
+      <p>
+        <b>"There are no tags!"</b>
+      </p>
+    ) : (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  }
 }
 
 export default Counter;
