ccd44e0f891aba61ecc60979234226f71c9718e5
ohad <ohad@ohads-Mac-mini.local>
2020-11-02 17:46:33 +0200

    started adding flask api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css'
-import Counters from './components/counters'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import Counters from "./components/counters";
+// import timeNow from "./components/timenow";
 
 ReactDOM.render(
   <React.StrictMode>
     <Counters />
+    {/* <timeNow /> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
